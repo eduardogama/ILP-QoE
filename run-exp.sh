e85308a785c41df682a169623ccc9e1ff89b3187
Eduardo Gama <eduardogama72@gmail.com>
2021-11-09 12:24:58 -0300

    .

---
+++ run-exp.sh
@@ -0,0 +1,11 @@
+
+# python3.7 cflp-2.py <# Groups> 
+#					  <Ap> <Group Id> <Groupd Index> <Group Size> ... 
+
+
+python3.7 cflp-2.py 5 0 1    \
+					3 8  0 5 \
+					4 9  1 1 \
+					5 10 2 1 \
+					3 11 3 2 \
+					6 12 4 3
